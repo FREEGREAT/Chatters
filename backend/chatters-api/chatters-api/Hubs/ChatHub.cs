32081b2a3150e61a23ad0a3f830a420bc6c98a52
FREEGREAT <vladdmitrenko3009@gmail.com>
2025-10-26 14:22:54 +0200

    Base realisation SignalR

---
+++ backend/chatters-api/chatters-api/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using chatters_api.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace chatters_api.Hubs;
+
+public interface IChatClient
+{
+    public Task RecieveMessage(string userName, string message);
+}
+
+public class ChatHub:Hub<IChatClient>
+{
+    public async Task JoinChat(UserConnection connection)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+
+        await Clients.Group(connection.ChatRoom).RecieveMessage("System", $"{connection.UserName}");
+    }
+}
